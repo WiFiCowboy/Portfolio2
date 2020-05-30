75d3ae458b797349e51f593e80c11e436df71683
Humberto Hernandez <hamburger45@gmail.com>
2020-05-30 15:10:02 -0700

    updated style and response

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 // Vanilla JS
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
-
-navBarToggle.addEventListener('click', function () {
-    mainNav.classList.toggle('active');
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
 });
 
-// jQuery DOM manipulation  
-function collapseNav(){
-    $('.nav-links').click(function(){
-        $('ul').removeClass('active'); 
-    });
-};
+// jQuery DOM manipulation
+function collapseNav() {
+  $(".nav-links").click(function () {
+    $("ul").removeClass("active");
+  });
+}
 
-function masterControl(){
-    collapseNav();
-};
+function masterControl() {
+  collapseNav();
+}
 
 $(masterControl);
